b32932ae953d23a8743526071949d05ef7cf8270
arek <arekrucki@hotmail.co.uk>
2021-07-12 17:11:51 +0100

    added new route with methods GET

--- controllers/transactions_controller.py
+++ controllers/transactions_controller.py
@@ -8,8 +8,17 @@ import repositories.tag_repository as tag_repository
 transactions_blueprint = Blueprint("transactions", __name__)
 
 
+
+
+
 @transactions_blueprint.route("/transactions")
 def transactions():
     transactions = transaction_repository.select_all()
-    return render_template("transactions/index.html", )
-    
+    return render_template("transactions/index.html")
+
+
+@transactions_blueprint.route("/transactions/new", methods=["GET"])
+def new_transaction():
+    transactions = transaction_repository.select_all()
+    return render_template("transactions/new.html")
+
